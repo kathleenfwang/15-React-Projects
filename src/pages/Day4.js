48587e84189682708169e5468545225154a6ec41
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-09-24 10:40:37 -0700

    day4

--- src/pages/Day4.js
+++ src/pages/Day4.js
@@ -4,6 +4,7 @@ export default function Day4() {
     const [mouseX,setMouseX] = useState(0)
     const [mouseY,setMouseY] = useState(0)
     const [text,setText] = useState("")
+    const [textList,setTextList] = useState([])
     useEffect(() => {
         soTired()
         document.addEventListener("mousemove", (e) => {
@@ -14,7 +15,7 @@ export default function Day4() {
     })
     let style = {
         display:"grid",
-        gridTemplateColumns:"1fr 1fr 1fr 1fr"
+        gridTemplateColumns:"1fr 1fr 1fr"
     }
 
     function mouseStyle(num) {
@@ -32,20 +33,58 @@ export default function Day4() {
       
         }
     }
-    function soTired(e){
-        console.log(e)
+    function handleChange(e) {
+        setText(e.target.value)
     }
+    function soTired(e){
+ 
+      if (e) {
+          if (e.key === "Enter") {
+              e.preventDefault() // this prevents skpping new line
+              let value = e.target.value
+              setText("")
+              setTextList((prevText) => (
+                  [...prevText,value]
+              ))
+          }
+      }
+     
+    }
+    function putTextList() {
+        return textList.map((text) => {
+            return (
+                <div>
+                    <p>{text}</p>
+                </div>
+            )
+        })
+    }
+    function clear() {
+        setText("")
+        setTextList([])
+    }
+    function downloadTxtFile(){
+        const element = document.createElement("a");
+        const file = new Blob(textList, {type: 'text/plain'});
+        element.href = URL.createObjectURL(file);
+        element.download = "feelings.txt";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+      }
     return(
         <div className ="day4">
              <h1>FEELINGS </h1>
-             <textarea placeholder ="How are you feeling?"onSubmit = {soTired}></textarea>
-             <button  type="submit">submit</button>
+             
+             <textarea id ="myInput" autoFocus placeholder ="How are you feeling?" value ={text} onChange = {handleChange} onKeyPress = {soTired}></textarea>
+             <br></br>
+            <button onClick ={clear}>Clear </button>
+            <button onClick ={downloadTxtFile}>Save as .txt</button>
              <div style ={mouseStyle(1)}>
         </div>
         <div style ={mouseStyle(3)}>
         </div>
         <div style = {style}>
-           {text}
+            {putTextList()}
         </div>
     
         </div>
