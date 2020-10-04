3b81dc9df846854e93b5e6fe3f3b4d906d4f2db3
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-10-04 16:46:33 -0700

    filtering for day9

--- src/pages/Day9.js
+++ src/pages/Day9.js
@@ -4,15 +4,15 @@ export default class Day9 extends React.Component {
     constructor() {
         super()
         this.state = {
-            show: true,
             likes: [],
             active: [],
-            filtered: null,
-            promptList: false
+            filtered: [],
+            activeList: 0
         }
+        this.promptListUrl = 'https://media.discordapp.net/attachments/701277128951595030/762183039857065994/official-inktober-2020-prompt-list.png?width=461&height=461'
         this.srcs = ['https://media.discordapp.net/attachments/701277128951595030/762123087553888307/Shape_4.png?width=347&height=490', 'https://media.discordapp.net/attachments/701277128951595030/762123167476613130/Shape_5.png?width=355&height=489', 'https://media.discordapp.net/attachments/701277128951595030/762116290059698186/imgonline-com-ua-twotoone-DRi4gTlNCiK6Jvq.jpg?width=403&height=457',]
         this.titles = ['fish', 'wisp', 'bulky',]
-        this.promptListUrl = 'https://media.discordapp.net/attachments/701277128951595030/762183039857065994/official-inktober-2020-prompt-list.png?width=461&height=461'
+
     }
     makeCards = (notLikes) => {
         const { active } = this.state
@@ -41,46 +41,64 @@ export default class Day9 extends React.Component {
             }))
         }
     }
-    handleToggle = () => {
-        this.setState((prevState) => ({
-            show: !prevState.show, 
-            promptList: false
-        }))
+    handleToggle = (i) => {
+        this.setState({
+            activeList: i
+        })
     }
     handleInput = (e) => {
-        let value = e.target.value
-        let days = this.titles.length
-        if (this.titles.includes(value.toLowerCase()) || (value >= 0 && value <= days)) {
-            if (Number(value)) {
-
-                this.setState({
-                    filtered: Number(value) - 1
-                }
-                )
-            }
-            else {
-
-                this.setState({
-                    filtered: this.titles.indexOf(value) !== -1 ? this.titles.indexOf(value) : null
-                })
-            }
+        let value = e.target.value !== "" ? e.target.value : null
+        if (this.titles[0].includes(value)) {
+            this.setState({
+                filtered: this.titles.reduce((prev, next, i) => {
+                    if (next.includes(value)) prev.push(i)
+                    return prev
+                }, [])
+            })
+        }
+        else if (this.titles[Number(value) - 1]) {
+            this.setState({
+                filtered: [Number(value) - 1]
+            })
         }
         else {
             this.setState({
-                filtered: null,
-                promptList: false
+                filtered: []
             })
         }
     }
-    handlePromptList = () => {
-        this.setState(prevState => ({
-            promptList: !prevState.promptList
-        }))
+    getList = () => {
+        let list = ['All', 'Likes', 'Prompt List']
+        const { activeList } = this.state
+        return list.map((x, i) => {
+            return (
+                <li i={i} style={{ borderBottom: activeList == i ? "2px solid pink" : "none" }} onClick={() => this.handleToggle(i)} >{x}</li>
+            )
+        })
     }
-   
+    items = () => {
+        const { activeList, filtered, likes } = this.state
+        switch (activeList) {
+            case 0:
+                return (
+                    <div className="flex center">
+                        {this.makeCards(true)}
+                    </div>)
+            case 1:
+                return (
+                    <div className="flex center">
+                        {likes.length > 0 ? likes :
+                            <h3>Empty :( Click the heart icon to like!</h3>}
+                    </div>)
+            case 2:
+                return (
+                    <div className="flex center down">
+                        <img src={this.promptListUrl} />
+                    </div>)
+        }
+    }
     render() {
-        const { show, likes, filtered, promptList } = this.state
-        console.log(promptList)
+        const { filtered } = this.state
         return (
             <div className="day9">
                 <div className="flex">
@@ -88,30 +106,15 @@ export default class Day9 extends React.Component {
                     <h4>I drew these with pen and then scanned into my computer using Adobe Capture :) </h4>
                 </div>
                 <nav className="nav center " >
-                    <li style={{ borderBottom: show ? "2px solid pink" : "none" }} onClick={this.handleToggle}>All</li>
-                    <li style={{ borderBottom: show ? "none" : "2px solid pink" }} onClick={this.handleToggle}>Likes</li>
-                    {/* <li onClick={this.handlePromptList}>Prompt List</li> */}
+                    {this.getList()}
                     <li><input placeholder="Search by day or name..." onChange={this.handleInput}></input></li>
-
                 </nav>
-                { promptList ?
-                    <div className ="flex center down">
-                        <img src={this.promptListUrl} />
-                    </div> : filtered || filtered == 0 ?
-                        <div className="flex center">
-                            {this.makeCards(true)[filtered]}
-                        </div>
-                        :
-                        show ? <div className="flex center">
-                            {this.makeCards(true)}
-                        </div>
-                            :
-                            <div>
-                                <div className="flex center">
-                                    {likes.length > 0 ? likes : <h3>Empty :( Click the heart icon to like!</h3>}
-                                </div>
-                            </div>
-                }
+                {filtered.length > 0 ?
+                    <div className="flex center">
+                        {this.makeCards(true).filter((x, i) => {
+                            return filtered.includes(i)
+                        })}
+                    </div> : this.items()}
             </div>
         )
     }
