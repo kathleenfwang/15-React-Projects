04458c7ab0ca913b45bc0b7a219edab041a0e49f
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-10-05 11:26:00 -0700

    day 9 titles restructure

--- src/pages/Day9.js
+++ src/pages/Day9.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Card from "./Components1/day9/Card"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarTimes} from '@fortawesome/free-solid-svg-icons'
 export default class Day9 extends React.Component {
     constructor() {
         super()
@@ -8,19 +10,47 @@ export default class Day9 extends React.Component {
             active: [],
             filtered: [],
             activeList: 0,
-            titles: ['fish', 'wisp', 'bulky', 'radio']
         }
-        this.promptListUrl = 'https://media.discordapp.net/attachments/701277128951595030/762183039857065994/official-inktober-2020-prompt-list.png?width=461&height=461'
-        this.srcs = ['https://media.discordapp.net/attachments/701277128951595030/762123087553888307/Shape_4.png?width=347&height=490', 'https://media.discordapp.net/attachments/701277128951595030/762123167476613130/Shape_5.png?width=355&height=489', 'https://media.discordapp.net/attachments/701277128951595030/762116290059698186/imgonline-com-ua-twotoone-DRi4gTlNCiK6Jvq.jpg?width=403&height=457', 'https://media.discordapp.net/attachments/701277128951595030/762471917680459786/Shape_9.png?width=327&height=491']
+        this.promptListUrl = 'https://media.discordapp.net/attachments/701277128951595030/762183039857065994/official-inktober-2020-prompt-list.png?width=490&height=490'
 
+        this.titles = [
+            {
+                title: 'fish',
+                src: 'https://media.discordapp.net/attachments/701277128951595030/762123087553888307/Shape_4.png?width=347&height=490',
+            },
+            {
+                title: 'wisp',
+                src: 'https://media.discordapp.net/attachments/701277128951595030/762123167476613130/Shape_5.png?width=355&height=489',
+            },
+            {
+                title: 'bulky',
+                src: 'https://media.discordapp.net/attachments/701277128951595030/762116290059698186/imgonline-com-ua-twotoone-DRi4gTlNCiK6Jvq.jpg?width=403&height=457',
+            },
+            {
+                title: 'radio',
+                src: ['https://media.discordapp.net/attachments/701277128951595030/762471917680459786/Shape_9.png?width=327&height=491', 'https://media.discordapp.net/attachments/519058439339507743/762476509633576990/Shape_10.png?width=340&height=489'],
+            },
+
+        ]
 
     }
     makeCards = (notLikes) => {
-        const { active, titles } = this.state
-        return titles.map((title, i) => {
-            return (
-                <Card key={i} notLikes={notLikes} active={active} i={i} handleLike={this.handleLike} src={this.srcs[i]} day={i + 1} title={title} />)
-        })
+        const { active } = this.state
+        let cards = []
+        let index = 0
+        let titles = this.titles[0]
+        for (let key in titles) {
+            if (Array.isArray(titles[key])) {
+                cards.push(...titles[key].map((x,i) => {
+                    return <Card notLikes={notLikes} active={active} i={index + i} handleLike={this.handleLike} src={x} day={index + 1} title={`${key} ${i+1}`} />
+                }))
+            }
+            else {
+                cards.push(<Card notLikes={notLikes} active={active} i={index} handleLike={this.handleLike} src={titles[key]} day={index + 1} title={key} />)
+            }
+            index++
+        }
+        return cards
     }
     handleLike = (i) => {
         const { active } = this.state
@@ -48,12 +78,21 @@ export default class Day9 extends React.Component {
         })
     }
     handleInput = (e) => {
-        const { titles } = this.state
+        let titles = Object.keys(this.titles[0])
         let value = e.target.value !== "" ? e.target.value : null
         if (titles.join().includes(value)) {
             this.setState({
                 filtered: titles.reduce((prev, next, i) => {
-                    if (next.includes(value)) prev.push(i)
+                    if (next.includes(value)) 
+                    {   
+                         if (Array.isArray(this.titles[0][next])) {
+                             this.titles[0][next].forEach((x,ind) => {
+                                 prev.push(i + ind) })
+                         }
+                         else {
+                         prev.push(i) 
+                        }
+                    }
                     return prev
                 }, [])
             })
@@ -74,7 +113,7 @@ export default class Day9 extends React.Component {
         const { activeList } = this.state
         return list.map((x, i) => {
             return (
-                <li i={i} className ="navList" style={{ borderBottom: activeList == i ? "2px solid #E0C3FC" : "none" }} onClick={() => this.handleToggle(i)} >{x}</li>
+                <li i={i} className="navList" style={{ borderBottom: activeList == i ? "2px solid #E0C3FC" : "none" }} onClick={() => this.handleToggle(i)} >{x}</li>
             )
         })
     }
@@ -99,14 +138,20 @@ export default class Day9 extends React.Component {
                     </div>)
             case 3:
                 return (
-                    <div style ={{marginTop: 40}}>
+                    <div style={{ marginTop: 40 }}>
                         <h4>I drew these with pen and then scanned using Adobe Capture :) </h4>
                         <p>Honestly this is my first year actually really trying to commit the whole month of Inktober. <br></br>Every other year I tell myself that but end up only completing the first week. <br></br>But what better time than quarantine to start.</p>
                         <p>In the future, I am planning on converting this into a full stack app with database storage and authentication, to allow authenticated users to upload their entries and like and comment on others. <br></br>It would be a fun way to encourage my friends to participate and share our progress with each other.</p>
-                        <p><a href ="https://inktober.com/" target ="_blank">Official Inktober Website</a></p>
+                        <p><a href="https://inktober.com/" target="_blank">Official Inktober Website</a></p>
                     </div>
                 )
         }
+    }
+    handleClear = () => {
+        this.setState({
+            filtered: [] 
+        })
+        this.refs.input.value = ""
     }
     render() {
         const { filtered } = this.state
@@ -117,7 +162,7 @@ export default class Day9 extends React.Component {
                 </div>
                 <nav className="nav center " >
                     {this.getList()}
-                    <li><input placeholder="Search by day or name..." onChange={this.handleInput}></input></li>
+                    <li><input ref = "input" placeholder="Search by day or name..." onChange={this.handleInput}></input><FontAwesomeIcon onClick = {this.handleClear} icon = {faCalendarTimes}/></li>
                 </nav>
                 {filtered.length > 0 ?
                     <div className="flex center">
