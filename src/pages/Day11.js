1fb27ca2d82c89ae7375c1260b5553f548ce3231
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-10-07 22:55:18 -0700

    day11

--- src/pages/Day11.js
+++ src/pages/Day11.js
@@ -42,7 +42,10 @@ export default class Day11 extends React.Component {
         return <h2>Your size isn't a book, don't judge it</h2>
         case 4: 
         return <h2>Your life isn't a film, don't end it</h2>
-
+        case 5: 
+        return (<div><h2>National Suicide Prevention Lifeline</h2>
+            <p style ={{fontSize:'1.2em', fontFamily:'Arial'}}>Available 24 hours. Languages: English, Spanish. Learn more
+            800-273-8255</p></div>)
       }
    
   }
@@ -64,7 +67,9 @@ export default class Day11 extends React.Component {
             <Fade clear left cascade>
             <h2>{this.state.text && this.renderText(4)}</h2>
             </Fade>
-
+            <Fade clear bottom cascade>
+            {this.state.text && this.renderText(5)}
+            </Fade>
             </div>
             </div>
         )
