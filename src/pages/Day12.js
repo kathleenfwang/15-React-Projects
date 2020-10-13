9cda09f8e82e53a16aea205eb2bd5358ad857189
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-10-13 14:26:17 -0700

    day12

--- src/pages/Day12.js
+++ src/pages/Day12.js
@@ -1,15 +1,39 @@
 import React from "react"
-
+import heel from "./Components1/day12/heel.mp3"
+import sneaker from "./Components1/day12/sneaker.mp3"
+import girllaugh from "./Components1/day12/girllaugh.mp3"
 export default class Day12 extends React.Component { 
     constructor() {
         super() 
-        this.state = {} 
+        this.state = {
+            play: false,
+            pause: true,
+        } 
+        this.sneaker = new Audio(sneaker);
+        this.heel = new Audio(heel)
+        this.laugh = new Audio(girllaugh)
     }
-
+    componentDidMount() {
+        document.addEventListener("keydown", this.startFootsteps)
+        document.addEventListener("keyup", this.pauseFoosteps)
+    }
+    startFootsteps = (e) => {
+        let key = e.key 
+        if (key === "ArrowUp") {
+        this.sneaker.play();
+        setTimeout(() => {
+            this.laugh.play()
+        }, 2000)
+        }
+    }
+    pauseFoosteps = (e) => {
+        this.sneaker.pause()
+        this.laugh.pause()
+    }
     render(){
         return(
-            <div>
-                day12
+            <div className ="day12">
+                <h1>Press the "up" arrow key to start walking</h1>
             </div>
         )
     }
