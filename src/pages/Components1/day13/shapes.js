875560521502e4cb09d690f8e5aee433a331d648
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-10-14 16:43:15 -0700

    shapes

--- src/pages/Components1/day13/shapes.js
+++ src/pages/Components1/day13/shapes.js
@@ -24,8 +24,8 @@ var geometry6 = new THREE.RingGeometry( 1, 5, 32 );
 var material6 = new THREE.MeshNormalMaterial({wireframe:true,side: THREE.DoubleSide});
 var ring = new THREE.Mesh( geometry6, material6 );
 
-var geometry7 = new THREE.TetrahedronGeometry(false);
-var material7 = makeMaterials()
+var geometry7 = new THREE.TetrahedronGeometry();
+var material7 = makeMaterials(false)
 let tetrahedron = new THREE.Mesh(geometry7,material7)
 
 var geometry8 = new THREE.TorusGeometry( 10, 3, 16, 100 );
@@ -57,10 +57,34 @@ var geometry9 = new THREE.TubeGeometry( path, 20, 2, 8, false );
 var material9 = makeMaterials()
 var tube = new THREE.Mesh( geometry9, material9);
 
+var x = 0, y = 0;
+
+var heartShape = new THREE.Shape();
+
+heartShape.moveTo( x + 5, y + 5 );
+heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+var meshFunc = function(u0, v0, pos) {
+    pos.set (-100 + 200 * u0, 
+    3 * Math.sin(20 * (u0 + v0)), 
+    -100 + 200 * v0);
+  };
+var geometry10 = new THREE.ShapeGeometry( heartShape );
+var material10 = makeMaterials()
+var heart = new THREE.Mesh( geometry10, material10 ) ;
+
+var geometry11 = new THREE.ParametricGeometry(meshFunc, 40, 40);
+var material11 = makeMaterials()
+var parametric = new THREE.Mesh( geometry11, material11 );
 function makeMaterials(wireframe = true) {
     return new THREE.MeshNormalMaterial({ wireframe: wireframe });
 }
 
 let shapesInner = {icosahedron,dodecahedron,octahedron,tetrahedron,cone}
-let shapesOuter = {torusKnot,torus,ring,tube}
+let shapesOuter = {torusKnot,torus,tube,heart,parametric,ring}
 export {shapesInner,shapesOuter}
--- src/pages/Day13.js
+++ src/pages/Day13.js
@@ -1,6 +1,6 @@
 import React from "react"
 import * as THREE from 'three';
-import {shapesInner,shapesOuter} from "./Components1/day13/shapes"
+import { shapesInner, shapesOuter } from "./Components1/day13/shapes"
 var id = null;
 export default class Day13 extends React.Component {
     constructor() {
@@ -11,76 +11,79 @@ export default class Day13 extends React.Component {
         }
     }
     componentDidMount() {
-       
         this.startAnimate()
     }
-    componentDidUpdate(prevprops,prevState) {
-        const{value1,value2} = this.state
-        if (value1 !==prevState.value1 || value2 !== prevState.value2) {
+    componentDidUpdate(prevprops, prevState) {
+        const { value1, value2 } = this.state
+        if (value1 !== prevState.value1 || value2 !== prevState.value2) {
             this.startAnimate()
         }
     }
-startAnimate = () => {
-    const {value1,value2} = this.state
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    // delete previous dom 
-    this.removeAllChildNodes(this.refs.div)
-    this.refs.div.appendChild( renderer.domElement );
-    camera.position.z = 5;
-    let shapes = [shapesInner[value1],shapesOuter[value2]]
-    console.log(shapes)
-    shapes.forEach((shape) => scene.add(shape))
-    if (id !== null) 
-    cancelAnimationFrame(id)   
-    const animate = () => {
-        id = requestAnimationFrame( animate );
+    startAnimate = () => {
+        const { value1, value2 } = this.state
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // delete previous dom 
+        this.removeAllChildNodes(this.refs.div)
+        this.refs.div.appendChild(renderer.domElement);
+        camera.position.z = 5;
+        let shapes = [shapesInner[value1], shapesOuter[value2]]
+        console.log(shapes)
+        shapes.forEach((shape) => scene.add(shape))
+        if (id !== null)
+            cancelAnimationFrame(id)
+        const animate = () => {
+            id = requestAnimationFrame(animate);
             shapes.forEach((shape) => {
                 shape.rotation.x += 0.01
                 shape.rotation.y += 0.01
             })
-        renderer.render( scene, camera );
+            renderer.render(scene, camera);
+        }
+        animate()
     }
-    animate()
-}
 
-removeAllChildNodes = (parent) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+    removeAllChildNodes = (parent) => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
     }
-}
-handleChange1 = (e) =>{
-    this.setState({value1: e.target.value})
- }
- handleChange2 = (e) =>{
-     this.setState({value2: e.target.value})
- }
- 
+    handleChange1 = (e) => {
+        this.setState({ value1: e.target.value })
+    }
+    handleChange2 = (e) => {
+        this.setState({ value2: e.target.value })
+    }
+    setDropdowns = () => {
+        return (
+            <div className ="big center">
+                    <label>Choose an outer shape:</label>
+                    <select onChange={this.handleChange2}>
+                        <option value="torusKnot">Torus Knot</option>
+                        <option value="torus">Torus</option>
+                        <option value="tube">Tube</option>
+                        <option value="parametric">Parametric</option>
+                    </select>
+                    <label>Choose an inner shape:</label>
+                    <select onChange={this.handleChange1}>
+                        <option value="icosahedron">Icosahedron</option>
+                        <option value="tetrahedron">Tetrahedron</option>
+                        <option value="dodecahedron">Dodecahedron</option>
+                        <option value="octahedron">Octahedron</option>
+                        <option value="cone">Cone</option>
+                    </select>
+                </div>
+        )
+    }
     render() {
-        return(
+        return (
             <>
-              <div>
-              <label>Choose an outer shape:</label>
-<select onChange ={this.handleChange2}id="cars" name="cars">
-  <option value="torusKnot">Torus Knot</option>
-  <option value="torus">Torus</option>
-  <option value="ring">Ring</option>
-  <option value="tube">Tube</option>
-</select>
-              <label>Choose an inner shape:</label>
-<select onChange ={this.handleChange1}id="cars" name="cars">
-  <option value="icosahedron">Icosahedron</option>
-  <option value="dodecahedron">Dodecahedron</option>
-  <option value="octahedron">Octahedron</option>
-  <option value="tetrahedron">Tetrahedron</option>
-  <option value="cone">Cone</option>
-</select>
-              </div>
-            <div ref ="div"> </div>
-            
-              </>
+                {this.setDropdowns()}
+                <div style ={{overflowY:"hidden",overflow:"hidden"}} ref="div"> </div>
+                <p style ={{textAlign:"right"}}><i>Animated with Three.js</i></p>
+            </>
         )
     }
 }
