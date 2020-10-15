06837b0db5e426e55fc52b07c6661af8dae9a791
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-10-15 12:29:05 -0700

    formatting day13 shapes

--- src/pages/Components1/day13/shapes.js
+++ src/pages/Components1/day13/shapes.js
@@ -1,34 +1,34 @@
 import * as THREE from 'three';
+const basic = true, wireframe = false
+let geometry1 = new THREE.TorusKnotBufferGeometry(10, 3, 100, 16);
+let material1 = new THREE.MeshNormalMaterial({ wireframe: true, depthTest: false, side: THREE.BackSide });
+let torusKnot = new THREE.Mesh(geometry1, material1);
 
-var geometry1 = new THREE.TorusKnotBufferGeometry(10, 3, 100, 16);
-var material1 = new THREE.MeshNormalMaterial({ wireframe: true, depthTest: false, side: THREE.BackSide });
-var torusKnot = new THREE.Mesh(geometry1, material1);
-
-var geometry2 = new THREE.IcosahedronGeometry();
-var material2 = makeMaterials(false)
-var icosahedron = new THREE.Mesh(geometry2, material2);
+let geometry2 = new THREE.IcosahedronGeometry();
+let material2 = makeMaterials(basic,wireframe)
+let icosahedron = new THREE.Mesh(geometry2, material2);
 
 let geometry3 = new THREE.ConeGeometry();
-let material3 = makeMaterials(false)
+let material3 = makeMaterials(basic,wireframe)
 let cone = new THREE.Mesh(geometry3, material3)
 
 let geometry4 = new THREE.DodecahedronGeometry();
-let material4 = makeMaterials(false)
+let material4 = makeMaterials(basic,wireframe)
 let dodecahedron = new THREE.Mesh(geometry4,material4)
 
 let geometry5 = new THREE.OctahedronGeometry();
-let material5 = makeMaterials(false)
+let material5 = makeMaterials(basic,wireframe)
 let octahedron = new THREE.Mesh(geometry5,material5)
 
-var geometry6 = new THREE.RingGeometry( 1, 5, 32 );
-var material6 = new THREE.MeshNormalMaterial({wireframe:true,side: THREE.DoubleSide});
-var ring = new THREE.Mesh( geometry6, material6 );
+let geometry6 = new THREE.RingGeometry( 1, 5, 32 );
+let material6 = new THREE.MeshNormalMaterial({wireframe:true,side: THREE.DoubleSide});
+let ring = new THREE.Mesh( geometry6, material6 );
 
-var geometry7 = new THREE.TetrahedronGeometry();
-var material7 = makeMaterials(false)
+let geometry7 = new THREE.TetrahedronGeometry();
+let material7 = makeMaterials(basic,wireframe)
 let tetrahedron = new THREE.Mesh(geometry7,material7)
 
-var geometry8 = new THREE.TorusGeometry( 10, 3, 16, 100 );
+let geometry8 = new THREE.TorusGeometry( 10, 3, 16, 100 );
 let material8 = makeMaterials()
 let torus = new THREE.Mesh(geometry8,material8)
 function CustomSinCurve( scale ) {
@@ -44,22 +44,22 @@ CustomSinCurve.prototype.constructor = CustomSinCurve;
 
 CustomSinCurve.prototype.getPoint = function ( t ) {
 
-	var tx = t * 3 - 1.5;
-	var ty = Math.sin( 2 * Math.PI * t );
-	var tz = 0;
+	let tx = t * 3 - 1.5;
+	let ty = Math.sin( 2 * Math.PI * t );
+	let tz = 0;
 
 	return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
 
 };
 
-var path = new CustomSinCurve( 15 );
-var geometry9 = new THREE.TubeGeometry( path, 20, 2, 8, false );
-var material9 = makeMaterials()
-var tube = new THREE.Mesh( geometry9, material9);
+let path = new CustomSinCurve( 15 );
+let geometry9 = new THREE.TubeGeometry( path, 20, 2, 8, false );
+let material9 = makeMaterials()
+let tube = new THREE.Mesh( geometry9, material9);
 
-var x = 0, y = 0;
+let x = 0, y = 0;
 
-var heartShape = new THREE.Shape();
+let heartShape = new THREE.Shape();
 
 heartShape.moveTo( x + 5, y + 5 );
 heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
@@ -69,20 +69,20 @@ heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
 heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
 heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
 
-var meshFunc = function(u0, v0, pos) {
+let meshFunc = function(u0, v0, pos) {
     pos.set (-100 + 200 * u0, 
     3 * Math.sin(20 * (u0 + v0)), 
     -100 + 200 * v0);
   };
-var geometry10 = new THREE.ShapeGeometry( heartShape );
-var material10 = makeMaterials()
-var heart = new THREE.Mesh( geometry10, material10 ) ;
+let geometry10 = new THREE.ShapeGeometry( heartShape );
+let material10 = makeMaterials()
+let heart = new THREE.Mesh( geometry10, material10 ) ;
 
-var geometry11 = new THREE.ParametricGeometry(meshFunc, 40, 40);
-var material11 = makeMaterials()
-var parametric = new THREE.Mesh( geometry11, material11 );
-function makeMaterials(wireframe = true) {
-    return new THREE.MeshNormalMaterial({ wireframe: wireframe });
+let geometry11 = new THREE.ParametricGeometry(meshFunc, 40, 40);
+let material11 = makeMaterials()
+let parametric = new THREE.Mesh( geometry11, material11 );
+function makeMaterials(normal = true, wireframe = true) {
+    return normal ? new THREE.MeshNormalMaterial({ wireframe}) : new THREE.MeshBasicMaterial({color:"whitesmoke", wireframe})
 }
 
 let shapesInner = {icosahedron,dodecahedron,octahedron,tetrahedron,cone}
--- src/pages/Day13.js
+++ src/pages/Day13.js
@@ -1,7 +1,7 @@
 import React from "react"
 import * as THREE from 'three';
 import { shapesInner, shapesOuter } from "./Components1/day13/shapes"
-var id = null;
+
 export default class Day13 extends React.Component {
     constructor() {
         super()
@@ -9,6 +9,7 @@ export default class Day13 extends React.Component {
             value1: "icosahedron",
             value2: "torusKnot"
         }
+        this.id = null 
     }
     componentDidMount() {
         this.startAnimate()
@@ -21,21 +22,20 @@ export default class Day13 extends React.Component {
     }
     startAnimate = () => {
         const { value1, value2 } = this.state
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
+        let scene = new THREE.Scene();
+        let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 5;
+        let renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         // delete previous dom 
         this.removeAllChildNodes(this.refs.div)
         this.refs.div.appendChild(renderer.domElement);
-        camera.position.z = 5;
         let shapes = [shapesInner[value1], shapesOuter[value2]]
-        console.log(shapes)
         shapes.forEach((shape) => scene.add(shape))
-        if (id !== null)
-            cancelAnimationFrame(id)
+        if (this.id !== null)
+            cancelAnimationFrame(this.id)
         const animate = () => {
-            id = requestAnimationFrame(animate);
+            this.id = requestAnimationFrame(animate);
             shapes.forEach((shape) => {
                 shape.rotation.x += 0.01
                 shape.rotation.y += 0.01
@@ -58,31 +58,31 @@ export default class Day13 extends React.Component {
     }
     setDropdowns = () => {
         return (
-            <div className ="big center">
-                    <label>Choose an outer shape:</label>
-                    <select onChange={this.handleChange2}>
-                        <option value="torusKnot">Torus Knot</option>
-                        <option value="torus">Torus</option>
-                        <option value="tube">Tube</option>
-                        <option value="parametric">Parametric</option>
-                    </select>
-                    <label>Choose an inner shape:</label>
-                    <select onChange={this.handleChange1}>
-                        <option value="icosahedron">Icosahedron</option>
-                        <option value="tetrahedron">Tetrahedron</option>
-                        <option value="dodecahedron">Dodecahedron</option>
-                        <option value="octahedron">Octahedron</option>
-                        <option value="cone">Cone</option>
-                    </select>
-                </div>
+            <div className="big center">
+                <label>Choose an outer shape:</label>
+                <select onChange={this.handleChange2}>
+                    <option value="torusKnot">Torus Knot</option>
+                    <option value="torus">Torus</option>
+                    <option value="tube">Tube</option>
+                    <option value="parametric">Parametric</option>
+                </select>
+                <label>Choose an inner shape:</label>
+                <select onChange={this.handleChange1}>
+                    <option value="icosahedron">Icosahedron</option>
+                    <option value="tetrahedron">Tetrahedron</option>
+                    <option value="dodecahedron">Dodecahedron</option>
+                    <option value="octahedron">Octahedron</option>
+                    <option value="cone">Cone</option>
+                </select>
+            </div>
         )
     }
     render() {
         return (
             <>
                 {this.setDropdowns()}
-                <div style ={{overflowY:"hidden",overflow:"hidden"}} ref="div"> </div>
-                <p style ={{textAlign:"right"}}><i>Animated with Three.js</i></p>
+                <div style={{ overflowY: "hidden", overflow: "hidden" }} ref="div"> </div>
+                <p style={{ textAlign: "right" }}><i>Animated with Three.js</i></p>
             </>
         )
     }
