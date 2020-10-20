ed51b6352f5b98ad5e86ead103fb1b98734ff3b7
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-10-20 15:25:31 -0700

    inktober entry

--- src/pages/Components1/day9/inktoberEntries.js
+++ src/pages/Components1/day9/inktoberEntries.js
@@ -70,7 +70,7 @@ const inktoberEntries = [
     }, 
     {
         title: 'trap',
-        src: 'https://media.discordapp.net/attachments/701277128951595030/766882419256983582/Shape_38.png?width=340&height=490'
+        src: 'https://media.discordapp.net/attachments/701277128951595030/768235625378873375/Shape_41.png?width=340&height=490'
     }, 
     {
         title: 'dizzy', 
@@ -78,11 +78,12 @@ const inktoberEntries = [
     }, 
     {
         title: 'coral',
-        src: 'https://media.discordapp.net/attachments/701277128951595030/767249630886101002/Shape_45.png?width=340&height=490'
+        src: 'https://media.discordapp.net/attachments/701277128951595030/768237298511904790/Shape_50.png?width=340&height=490'
     }, 
     {title: 'sleep',
-    src: 'https://media.discordapp.net/attachments/701277128951595030/768186930159943700/Shape_47.png?width=340&height=490'
-}
+    src: 'https://media.discordapp.net/attachments/701277128951595030/768186930159943700/Shape_47.png?width=340&height=490'},
+ 
+
 ]
 const promptListUrl = 'https://media.discordapp.net/attachments/701277128951595030/762183039857065994/official-inktober-2020-prompt-list.png?width=490&height=490'
 
