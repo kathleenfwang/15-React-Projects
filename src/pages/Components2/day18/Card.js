afc7769c64359da932d327fdd29673c8df2426c0
Kathleen Wang <kathleen.wangibm.com@Kathleens-MBP.attlocal.net>
2020-11-06 19:00:12 -0800

    theme toggle day 18

--- src/pages/Components2/day18/Card.js
+++ src/pages/Components2/day18/Card.js
@@ -1,21 +1,47 @@
-import React from "react"
+import React, {useState} from "react"
 export default function Card({ testimony }) {
-    const cardStyle = {
-        borderRadius: 5,
-        padding: 20,
-        margin: 10,
-        border: '2px solid white',
-        width: '30%',
-        height: 500
+    let [color,setColor] = useState("") 
+    let [bg,setBgColor] = useState("") 
+    const getColor = () => {
+        switch(color){
+            case "black": 
+                bg = "#333"
+                break
+            case "red": 
+                bg = "palevioletred"
+                break
+            case "green":
+                bg = "darkgreen"
+                break
+        }
+        return {
+            backgroundColor: bg, 
+            color: color
+        }
+    }
+
+   
+    const handleTheme = (color) => {
+        setColor(color)
+    }
+    const getButtons = () => {
+        let colors = ['black','green','red']
+        return colors.map((color) => {
+            return <button onClick = {() => handleTheme(color)}>{color}</button>
+        })
     }
     return (
-        <div style={cardStyle}>
+        <div className ="cardStyle" style={getColor()}>
             <div className="textcenter">
                 <h1 >{testimony.name}</h1>
                 <h2>{testimony.status}</h2>
             </div>
             <h3> {testimony.highlight}</h3>
             <p>{testimony.desc}</p>
+        <div className ="flex">
+            <p>Change theme: </p>
+            {getButtons()}
+        </div>
         </div>
     )
 }
