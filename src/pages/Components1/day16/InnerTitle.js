ee614f84c11a788c26dd0e544d4db0748d0b0238
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-11-15 23:44:50 -0800

    dev jobs link

--- src/pages/Components1/day16/InnerTitle.js
+++ src/pages/Components1/day16/InnerTitle.js
@@ -13,13 +13,18 @@ export default function InnerTitle({data,theme}) {
     }
     const themeColor = theme ? "light" : "dark"
     const img = data.company_logo ? data.company_logo : "https://www.iconpacks.net/icons/2/free-briefcase-icon-1965-thumb.png"
+
+    function getUrl(str) {
+        if (str.length <= 25) return str 
+        return `${str.slice(0,25)}...`
+    }
     return (
         <div className ={`${themeColor}Container flex spaceBetween midwidth`}>
         <div className ="flex">
-        <div><img src ={img} style ={bgImg}/></div>
+        <div><a href = {data.company_url} target = "_blank"><img src ={img} style ={bgImg}/></a></div>
         <div style ={{lineHeight:0}}>
             <h1>{data.company}</h1>
-            <p>{data.company_url}</p>
+            <p>{getUrl(data.company_url)}</p>
         </div>
         </div>
         <div>
