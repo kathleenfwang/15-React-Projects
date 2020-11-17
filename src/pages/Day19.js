03d00c0a28e8cac37510ac0781bd49ead4c09413
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-11-17 13:45:05 -0800

    sample images colors

--- src/pages/Day19.js
+++ src/pages/Day19.js
@@ -7,6 +7,7 @@ export default class Day19 extends React.Component {
         this.state = {
             loaded: false,
             img: img,
+            photos: ['https://www.goodinfonet.com/uploads/news/goodinfonet_whats_up_wednesday_1596012821_0.jpg','https://www.rightstufanime.com/images/productImages/816546022563_anime-the-promised-neverland-blu-ray-primary.jpg?resizeid=3', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*'],
             colors: {}
         }
         this.url = process.env.REACT_APP_COMPVISION_URL
@@ -16,7 +17,13 @@ export default class Day19 extends React.Component {
     componentDidMount() {
         this.getData()
     }
+    componentDidUpdate(prevprops,prevState) {
+        if (prevState.img !== this.state.img) {
+            this.getData()
+        }
+    }
     getData = () => {
+        this.setState({loaded:false})
         const { img } = this.state
         let data =
             { "url": img }
@@ -55,23 +62,42 @@ export default class Day19 extends React.Component {
             }
         }
         return colorDetails.map((detail) => {
+            let detailText = ""
             if (detail === 'accentColor') {
-                colors[detail] = `#${colors[detail]}`
+                detailText = `#${colors[detail]}`
+            }
+            else {
+                detailText = colors[detail]
             }
 
             return (
                 <div className="flex">
                     <div className="square" style={{
-                        backgroundColor: colors[detail]
+                        backgroundColor: detailText
                     }} />
-                    <p>{colors[detail]}</p>
+                    <p>{detailText}</p>
                 </div>)
         })
     }
+    changePhoto = (photo) => {
+        this.setState({img: photo})
+    }
+    getPhotos = () => {
+        const {photos} = this.state 
+        return photos.map((photo) => {
+            return (<div className ="square" onClick ={() => this.changePhoto(photo)} style={{
+                backgroundImage: `url(${photo})`,
+                backgroundSize: "cover",}}></div>)
+        })
+    }
+    // getLoading = () => {
+    //     return (<img src ="https://i.gifer.com/YCZH.gif"/>)
+    // }
     render() {
         const { img, loaded } = this.state
         return (
             <div>
+                <div className ="flex">
                 <img style={{
                     width: 500,
                     height: 500,
@@ -79,7 +105,12 @@ export default class Day19 extends React.Component {
                 }} src={img} />
                 <div>
                     <p>Colors:</p>
-                    {loaded && this.getColors()}
+                    {loaded ? this.getColors() : 'Loading!'}
+                </div>
+            
+                </div>
+                <div className ='flex'>
+                    {this.getPhotos()}
                 </div>
             </div>
         )
