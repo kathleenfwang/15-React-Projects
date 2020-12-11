3248f4d3961a5d8b2d0deca5b06309b5fbe7d086
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-10 17:50:19 -0800

    nav option styling

--- src/pages/Components2/day20/NavOptions.js
+++ src/pages/Components2/day20/NavOptions.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function NavOptions(props) {
     const { tab,titles,functionName } = props
     const navTitles = titles.map((title, i) => {
-        return (  <li i={i} className="navList" style={{ fontSize: '1.2em', borderBottom: tab == i ? "2px solid #E0C3FC" : "2px solid transparent" }} onClick={() => functionName(i)} >{title}</li> )
+        return (  <li i={i} className="navList" style={{ fontSize: '1.3em', padding: 10, borderBottom: tab == i ? "2px solid #E0C3FC" : "2px solid transparent", boxShadow: tab == i ? ' 2px 4px 25px rgba(0, 0, 0, .1)' : 'none' }} onClick={() => functionName(i)} >{title}</li> )
     })
     return (
         <nav className="center">
