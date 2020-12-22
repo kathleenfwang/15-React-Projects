bb8e7185eee395d9c9d48f1c6bf2210cd2bf80da
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-22 11:47:51 -0800

    animal cards

--- src/pages/Components2/day22/Animals.js
+++ src/pages/Components2/day22/Animals.js
@@ -1,28 +1,45 @@
 import React from "react"
 import AnimalCard from "./AnimalCard"
 export default class Animals extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state ={}
+        this.state = {}
     }
-    getStuff =   () => {
-        const {data} = this.props 
-        console.log(data)
-        let results = [] 
-      setTimeout(() => { 
-            results = data.map((animal) => <AnimalCard data = {animal} />)
-            console.log(results)
-         }, 3000);
-         console.log(results)
  
-         return results
+    getAnimals = () => {
+        const { data } = this.props
+        // headers: ["ant", "cow", etc.. ]
+        let animalDic = {}
+        data.forEach((animal) => {
+            let type = animal.species
+            if (animalDic[type]) {
+                animalDic[type].push(animal)
+            }
+            else {
+                animalDic[type] = [animal]
+            }
+        })
+        console.log(animalDic)
+        return this.getAnimalCards(animalDic)
+
+    }
+    getAnimalCards = (animalDic) => {
+        let animalCards = []
+        for (let animal in animalDic) {
+            animalCards.push(
+                <div>
+                    <h1>{animal}</h1>
+                    <div className="flex">
+                        {animalDic[animal].map((animal) => <AnimalCard data={animal} />)}
+                    </div>
+                </div>)
+        }
+        return animalCards
     }
     render() {
-        const {data} = this.props
-       
-        return(
+        return (
             <div>
-                {this.getStuff() }
+                {this.getAnimals()}
             </div>
         )
     }
