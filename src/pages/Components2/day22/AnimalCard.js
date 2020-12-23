e388ed0cdc318385ea3a856ec2e9f5017633efef
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-22 16:05:55 -0800

    refractor

--- src/pages/Components2/day22/AnimalCard.js
+++ src/pages/Components2/day22/AnimalCard.js
@@ -1,64 +1,65 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTimes, faSmile, faArrowUp, faHeart } from '@fortawesome/free-solid-svg-icons'
 import React from "react"
+import { Fade } from 'react-reveal';
 export default class AnimalCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             clicked: false
-        } 
+        }
     }
     getImageData = () => {
-        this.setState(prevState => ({clicked: !prevState.clicked}))
+        this.setState(prevState => ({ clicked: !prevState.clicked }))
     }
     handleClick = (e) => {
-        this.setState({clicked: false})
+        this.setState({ clicked: false })
     }
     imageData = () => {
-        const {data} = this.props
-        const {clicked} = this.state
+        const { data } = this.props
+        const { clicked } = this.state
         return (
-            <div className ={`${clicked}Show floatingCard`}>
-                <div className ="up flex spaceBetween">
-            <h2>{data.name["name-USen"]}</h2>
-            <FontAwesomeIcon className ="cursor red" icon ={faTimes} onClick ={this.handleClick}/>
+            <div className={`${clicked}Show floatingCard`}>
+                <div className="up flex spaceBetween">
+                    <h2>{data.name["name-USen"]}</h2>
+                    <FontAwesomeIcon className="cursor red" icon={faTimes} onClick={this.handleClick} />
                 </div>
-            <img className ="bigSquare" src = {data["image_uri"]}/>
-            <div className ="flexVertical spaceAround">
-            <p>Birthday: {data["birthday-string"]}</p>
-            <div className ="flex">
-            <p style ={this.getColor(data.personality)}className ="smallTag upLess">{data.personality}</p>
-            <p style ={this.getHobbyColor(data.hobby)} className = "smallTag upLess">{data.hobby}</p>
-            </div>
-            <p className ="quote upLess">{`"${data.saying}"`}</p>
-            </div>
+                <img className="bigSquare" src={data["image_uri"]} />
+                <div className="flexVertical spaceAround">
+                    <p>Birthday: {data["birthday-string"]}</p>
+                    <div className="flex">
+                        <p style={this.getColor(data.personality)} className="smallTag upLess">{data.personality}</p>
+                        <p style={this.getHobbyColor(data.hobby)} className="smallTag upLess">{data.hobby}</p>
+                    </div>
+                    <p className="quote upLess">{`"${data.saying}"`}</p>
+                </div>
             </div>)
     }
     getColor = (personality) => {
-       
-        const {animalPersonalities} = this.props
+        const { animalPersonalities } = this.props
         const personalities = Object.keys(animalPersonalities) // ["Peppy", "Normal".. ]
         const personalityIndex = personalities.indexOf(personality)
-        const colors = {pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"]}
-        const color =  personalityIndex % colors.beach.length
-        return {backgroundColor: colors.beach[color],color:"rgb(99, 89, 89)"}
+        const colors = { pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"] }
+        const color = personalityIndex % colors.beach.length
+        return { backgroundColor: colors.beach[color], color: "rgb(99, 89, 89)" }
     }
     getHobbyColor = (hobby) => {
-        const {animalHobbies} = this.props
+        const { animalHobbies } = this.props
         const hobbies = Object.keys(animalHobbies)
         const hobbyIndex = hobbies.indexOf(hobby)
-        const colors = {pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"]}
+        const colors = { pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"] }
         const color = hobbyIndex % colors.pastel.length
-        return {backgroundColor: colors.pastel[color],color:"rgb(99, 89, 89)"}
+        return { backgroundColor: colors.pastel[color], color: "rgb(99, 89, 89)" }
     }
     render() {
-        const {data} = this.props
-        return(
-            <div className ="relative">
-                <img onClick ={this.getImageData} className ="cursor" src ={data.icon_uri}/>
-                {this.imageData()}
-               
-            </div>
+        const { data } = this.props
+        return (
+            <Fade cascade>
+                <div className="relative">
+                    <img onClick={this.getImageData} className="cursor" src={data.icon_uri} />
+                    {this.imageData()}
+                </div>
+            </Fade>
         )
     }
 }
--- src/pages/Components2/day22/Animals.js
+++ src/pages/Components2/day22/Animals.js
@@ -2,7 +2,6 @@ import React from "react"
 import AnimalCard from "./AnimalCard"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faSmile, faArrowUp, faHeart, faHamburger } from '@fortawesome/free-solid-svg-icons'
-import { faBluetooth } from "@fortawesome/free-brands-svg-icons"
 export default class Animals extends React.Component {
     constructor(props) {
         super(props)
@@ -10,95 +9,97 @@ export default class Animals extends React.Component {
             animalDic: {},
             animalPersonalities: {},
             animalHobbies: {},
-            loaded:false,
-            clickedHamburger:true,
+            loaded: false,
+            firstLoad: false,
+            finalLoad: false,
+            clickedHamburger: true,
         }
+        this.animalData = null
     }
     componentDidMount() {
         this.getAnimals()
-    }
- 
-    getAnimals = () => {
-
-        // headers: ["ant", "cow", etc.. ]
-        const animalDic = this.makeDictionary("species")
-        const animalPersonalities = this.makeDictionary("personality")
-        const animalHobbies = this.makeDictionary("hobby")
-        this.setState({animalDic, animalPersonalities, animalHobbies, loaded:true})
     }
     makeDictionary = (type) => {
         const { data } = this.props
         let animalDic = {}
         data.forEach((animal) => {
-            if (animalDic[animal[type]]) {
-                animalDic[animal[type]].push(animal)
-            }
-            else {
-                animalDic[animal[type]] = [animal]
-            }
+            if (animalDic[animal[type]]) animalDic[animal[type]].push(animal)
+            else { animalDic[animal[type]] = [animal] }
         })
         return animalDic
     }
-    getAnimalCards = () => {
-        const {animalDic,animalPersonalities, animalHobbies} = this.state
+    getAnimals = () => {
+        // headers: ["ant", "cow", etc.. ]
+        const animalDic = this.makeDictionary("species")
+        const animalPersonalities = this.makeDictionary("personality")
+        const animalHobbies = this.makeDictionary("hobby")
+        this.setState({ animalDic, animalPersonalities, animalHobbies, firstLoad: true }, () => this.getAnimalCards())
+    }
+    getAnimalCards = async () => {
+        const result = await this.makeAnimalCards()
+        this.animalData = result
+        this.setState({ loaded: true })
+    }
+    makeAnimalCards = () => {
+        const { animalDic, animalPersonalities, animalHobbies } = this.state
         let animalCards = []
         let i = 0
         for (let animal in animalDic) {
             animalCards.push(
-                <div id ={animal}>
-                    <h1 className="textCenter button" style ={this.getColor(i)}>{animal}</h1>
+                <div id={animal}>
+                    <h1 className="textCenter button" style={this.getColor(i)}>{animal}</h1>
                     <div className="flex center">
-                        {animalDic[animal].map((animal) => <AnimalCard  key ={animal.id} data={animal} animalPersonalities = {animalPersonalities} animalHobbies = {animalHobbies}/>)}
+                        {animalDic[animal].map((animal) => <AnimalCard key={animal.id} data={animal} animalPersonalities={animalPersonalities} animalHobbies={animalHobbies} />)}
                     </div>
                 </div>)
-                i++
-        }
+            i++
+        };
         return animalCards
     }
     handleHamburger = (e) => {
-        this.setState(prevState =>({clickedHamburger: !prevState.clickedHamburger}))
+        this.setState(prevState => ({ clickedHamburger: !prevState.clickedHamburger }))
     }
     getHeaderTags = () => {
-        const {animalDic} = this.state
+        const { animalDic } = this.state
         let i = 0
-        let buttons = [] 
+        let buttons = []
         for (let animal in animalDic) {
-            buttons.push(<div className ="bigger margin"><a href ={`#${animal}`}><button style ={this.getColor(i)}>{animal}</button></a></div>)
-            i++ 
+            buttons.push(<div className="bigger margin"><a href={`#${animal}`}><button style={this.getColor(i)}>{animal}</button></a></div>)
+            i++
         }
         return buttons
     }
     getColor = (i) => {
-        const colors = {pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"]}
+        const colors = { pastel: ["#C1A7FF", "#C2CBFF", "#C7FCBA", "#FDFEC9", "#FFD8B6", " #FEBCC2"], beach: ["#C8F69B", "#FFEEA5", "#FFCBA5", "#FFB1AF", " #D6D4FF", "#B3EEFF"] }
         const color = i % colors.beach.length
-        return {backgroundColor: colors.beach[color],color:"rgb(99, 89, 89)"}
+        return { backgroundColor: colors.beach[color], color: "rgb(99, 89, 89)" }
     }
     getArrow = () => {
         const arrowStyle = {
-            position:"fixed", 
-            right:20, 
-            bottom: 20
+            position: "fixed",
+            right: 30,
+            bottom: "30px",
         }
-        return <a href="#start" style ={arrowStyle}> <button><FontAwesomeIcon icon ={faArrowUp}/></button></a>
+        return <a href="#start" style={arrowStyle}> <button><FontAwesomeIcon icon={faArrowUp} /></button></a>
     }
     getFixedHeader = () => {
-        const {loaded,clickedHamburger} = this.state
+        const { loaded, clickedHamburger } = this.state
         return (
-        <div className ="flex fixedHeader">
-        <div><button style ={{width:40}}className ="cursor" onClick ={this.handleHamburger}><FontAwesomeIcon icon ={faHamburger}/></button></div>
-        <div className ={`flex ${clickedHamburger}Show`}>
-        {loaded && this.getHeaderTags()}
-        </div>
-        </div> )
+            <div className="flex fixedHeader">
+                <div><button style={{ width: 40 }} className="cursor" onClick={this.handleHamburger}><FontAwesomeIcon icon={faHamburger} /></button></div>
+                <div className={`flex ${clickedHamburger}Show`}>
+                    {loaded && this.getHeaderTags()}
+                </div>
+            </div>)
     }
     render() {
-        const {loaded,clickedHamburger} = this.state
-        console.log(clickedHamburger)
+        const { loaded } = this.state
+        console.log(loaded, this.animalData)
         return (
-            <div className ="day22" id ="start">
+            <div className="day22" id="start">
                 {this.getFixedHeader()}
-                <div className ="downHeader">
-                {loaded && this.getAnimalCards()}
+                <div className="downHeader">
+                    {loaded && this.animalData}
                 </div>
                 {this.getArrow()}
             </div>
