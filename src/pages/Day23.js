e9c504aad3ef79b86e05882d2fac6857461c0635
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-01-06 09:59:26 -0800

    styling

--- src/pages/Day23.js
+++ src/pages/Day23.js
@@ -44,7 +44,7 @@ export default function Day23() {
     <ApolloProvider client={client}>
       <div>
         <h2>Currency Exchange Apollo App 🚀</h2>
-        <h3> Exchange rates for 1 USD (USA) </h3> 
+        <h3 style ={{backgroundColor: "yellow", width:300, padding:10}}> Exchange rates for 1 USD (USA) </h3> 
         <ExchangeRates />
       </div>
     </ApolloProvider>
