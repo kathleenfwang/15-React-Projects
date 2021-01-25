c6f979e1adf3c124f02b80f85e712a384741765c
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-01-25 13:59:19 -0800

    filtering

--- src/pages/Day24.js
+++ src/pages/Day24.js
@@ -16,16 +16,27 @@ export default class Day24 extends React.Component {
     getCountries = (countries) => {
         return countries.map((country) => <p> {country} </p>)
     }
+    checkCountry(country,value) {
+        // value = "an" 
+        // country = "Antigua" 
+        let i = 0 
+        let j = 0 
+        while (i < value.length && j < country.length) {
+            if (value[i] !== country[j]) return false 
+            i+=1
+            j+=1 
+        } 
+        return true
+    }
     handleInput =(e) => {
         let value = e.target.value 
         const {countries} = this.state 
-    
         // filtered = ["Andorra, Angola, Antigua"] 
-
         let filtered = countries.filter((country) => {
             // only want the beginning letters to match 
-            country = country.toLowerCase()
-            return country.slice(0,value.length) == value 
+            return this.checkCountry(country.toLowerCase(),value)
+            // country = country.toLowerCase()
+            // return country.slice(0,value.length) == value 
         })
         console.log(filtered)
         this.setState({filtered:filtered})
